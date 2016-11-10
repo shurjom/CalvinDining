08ec12ad6977e2c5ba20a3f8f6dcb89cbfbf7c94
shurjom <shurjom@gmail.com>
2016-11-10 01:31:55 -0500

    fixed

--- app/src/main/java/edu/calvin/kpb23students/calvindining/MainActivity.java
+++ app/src/main/java/edu/calvin/kpb23students/calvindining/MainActivity.java
@@ -97,22 +97,22 @@ public class MainActivity extends AppCompatActivity
      * @param item action bar item pressed
      * @return
      */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
-            startActivity(i);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
+//            startActivity(i);
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     /**
      * Handle menu items when they are pressed.
