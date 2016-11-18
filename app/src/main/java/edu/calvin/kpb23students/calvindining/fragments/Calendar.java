ef1b906e4bf54fd55a579b957055fbbdefe98b3c
Krisofer Brink <kpgbrink@gmail.com>
2016-11-17 22:10:49 -0500

    Revert "Meal plan fragment shows up but is non functional"
    This reverts commit b25f57fd501dd414955b0a295e79f037209f348f.

--- app/src/main/java/edu/calvin/kpb23students/calvindining/fragments/Calendar.java
+++ app/src/main/java/edu/calvin/kpb23students/calvindining/fragments/Calendar.java
@@ -7,10 +7,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-import android.view.View.OnClickListener;
 
 import edu.calvin.kpb23students.calvindining.R;
 
@@ -21,93 +17,32 @@ import edu.calvin.kpb23students.calvindining.R;
  *     This fragment shows the calendar
  * </p>
  */
-public class Calendar extends Fragment  {
+public class Calendar extends Fragment {
     public Calendar() {
         // Required empty public constructor
     }
 
-    public class FoodCount {
-
-        public int myCount;
-
-        public FoodCount(int value){this.myCount = value;}
-
-        void changeCount(int newCount){this.myCount = newCount;}
-
-        int getCount(){
-            return this.myCount;
-        }
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Calendar.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Calendar newInstance(String param1, String param2) {
+        Calendar fragment = new Calendar();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar, container, false);
-    }}
-
-    /*@Override
-    public void onActivityCreated(Bundle savedInstanceState){
-
     }
-        View v = getView();
-        Button configureButton = (Button) v.findViewById(R.id.foodButton);
-        Button eatButton = (Button)v.findViewById(R.id.consumeButton);
-        Button undoButton = (Button)v.findViewById(R.id.undoButton);
-
-
-        configureButton.setOnClickListener(new View.OnClickListener() {
-
-                    public void onClick(View v) {
-                        EditText foodCount = (EditText) findViewById(R.id.countText);
-                        int value = Integer.parseInt(foodCount.getText().toString());
-                        blockPlan.changeCount(value);
-                        Toast toast = Toast.makeText(getApplicationContext(),"Block count set to "+value,Toast.LENGTH_SHORT);
-                        toast.show();
-                    }
-                }
-        );
-
-        eatButton.setOnClickListener(
-                new Button.OnClickListener(){
-                    public void onClick(View v) {
-                        mealCount = blockPlan.getCount();
-                        foodPD++;
-                        mealCount--;
-                        if(mealCount < 0){
-                            foodPD--;
-                            mealCount = 0;
-                            Toast toast = Toast.makeText(getApplicationContext(),"Cannot put in anymore meals. Please add more blocks.",Toast.LENGTH_SHORT);
-                            toast.show();
-                        }
-                        blockPlan.changeCount(mealCount);
-                        TextView foodPDCount = (TextView) findViewById(R.id.foodPDView);
-                        EditText foodCount = (EditText) findViewById(R.id.countText);
-                        foodPDCount.setText(Integer.toString(foodPD));
-                        foodCount.setText(Integer.toString(mealCount));
-                    }
-                }
-        );
-
-        undoButton.setOnClickListener(
-                new Button.OnClickListener(){
-                    public void onClick(View v) {
-                        mealCount = blockPlan.getCount();
-                        foodPD--;
-                        mealCount++;
-                        if(foodPD < 0){
-                            foodPD++;
-                            mealCount--;
-                            Toast toast = Toast.makeText(getApplicationContext(),"Nothing to undo.",Toast.LENGTH_SHORT);
-                            toast.show();
-                        }
-                        blockPlan.changeCount(mealCount);
-                        TextView foodPDCount = (TextView) findViewById(R.id.foodPDView);
-                        EditText foodCount = (EditText) findViewById(R.id.countText);
-                        foodPDCount.setText(Integer.toString(foodPD));
-                        foodCount.setText(Integer.toString(mealCount));
-                    }
-                }
-        );
-}*/
-
+}
